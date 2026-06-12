866ba9292054f65f28eaf9fafb3a784c9287f4dc
karan-codess <ksy70a@gmail.com>
2026-06-12 13:13:23 +0530

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/shared/LandingPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/shared/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {landingPageStyles as s} from '../../assets/dummyStyles' 
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
