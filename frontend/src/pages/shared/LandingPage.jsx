53293a1d40b03cd8bcac2cb98c938f02a1d375d8
karan-codess <ksy70a@gmail.com>
2026-06-17 23:49:29 +0530

    logo done

--- frontend/src/pages/shared/LandingPage.jsx
+++ frontend/src/pages/shared/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {landingPageStyles as s} from '../../assets/dummyStyles' 
+import Navbar from '../../components/common/Navbar'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className={s.bgMain}>
+      <Navbar />
+    </div>
   )
 }
 
